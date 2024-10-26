21b7cfcb4c5eaa0a9507f5e740526ebb1ca6550c
GOUKUN5160 <gouichirou.yamada@gmail.com>
2024-10-26 10:14:19 +0900

    added loading display

--- Extension/.history/background_20241026100414.js
+++
@@ -1,3 +0,0 @@
-const setBadge = (text) => {
-  chrome.action.setBadgeText({ "text": text });
-};
--- Extension/.history/background_20241026100615.js
+++
@@ -1,9 +0,0 @@
-const setBadge = (text) => {
-  chrome.action.setBadgeText({ "text": text });
-};
-
-chrome.runtime.onMessage.addListener((request) => {
-  if (request.name === "setBadge") {
-    setBadge(request.text);
-  }
-});
